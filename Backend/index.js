9523a89c4d13c342694d42d1f2c78fdc8d9deb00
Pratik-Bavche <pratikbavche2005@gmail.com>
2025-10-05 08:28:23 +0530

    new  changes in backend

--- Backend/index.js
+++ Backend/index.js
@@ -16,9 +16,14 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"], // added 5175
+    origin: ["http://localhost:3000", "http://localhost:5173","http://localhost:5174","http://localhost:5175"],
     credentials: true
 }));
+
+// Root route
+app.get("/", (req, res) => {
+    res.json({ message: "Server is running successfully!" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
